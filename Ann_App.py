6a1eb83d51812896c373f2b52d8fafb68c2e0d75
Hrushikesh Mahesh Chavan <122804969+HrushikeshSS10@users.noreply.github.com>
2026-03-06 15:14:00 +0530

    Add files via upload

--- Ann_App.py
+++ Ann_App.py
@@ -7,7 +7,7 @@ import tensorflow as tf
 import pickle
 
 # Load the trained models
-model = tf.keras.models.load_model("model.keras", compile=False)
+model = tf.keras.models.load_model("model.h5", compile=False)
 
 # Load the encoders and scaler
 with open('label_encoder_gender.pkl', 'rb')as file:
