0cad895737be71c63bda60a9af45944ca3a90f75
Hrushikesh Mahesh Chavan <122804969+HrushikeshSS10@users.noreply.github.com>
2026-03-06 15:37:16 +0530

    Add files via upload

--- Ann_App.py
+++ Ann_App.py
@@ -12,7 +12,7 @@ from tensorflow.keras.layers import Dense
 
 def build_model():
     model = Sequential([
-        Dense(64, activation='relu', input_shape=(11,)),
+        Dense(64, activation='relu', input_shape=(12,)),
         Dense(32, activation='relu'),
         Dense(1, activation='sigmoid')
     ])
