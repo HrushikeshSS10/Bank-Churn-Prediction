801465b818e700d0ac24dec169dd9acc0cc7b838
Hrushikesh Mahesh Chavan <122804969+HrushikeshSS10@users.noreply.github.com>
2026-03-06 18:05:14 +0530

    Add files via upload

--- Ann_App.py
+++ Ann_App.py
@@ -2,7 +2,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import pickle
+import joblib
 import tensorflow as tf
 from sklearn.preprocessing import LabelEncoder, StandardScaler, OneHotEncoder
 from tensorflow.keras.models import Sequential
@@ -29,14 +29,9 @@ model.load_weights("model.weights.h5")
 
 # Load the encoders and scaler
 # Load encoders and scaler
-with open("label_encoder_gender.pkl", "rb") as file:
-    label_encoder_gender = pickle.load(file)
-
-with open("onehot_encoder_geo.pkl", "rb") as file:
-    onehot_encoder_geo = pickle.load(file)
-
-with open("scaler.pkl", "rb") as file:
-    scaler = pickle.load(file)
+label_encoder_gender = joblib.load("label_encoder_gender.joblib")
+onehot_encoder_geo = joblib.load("onehot_encoder_geo.joblib")
+scaler = joblib.load("scaler.joblib")
     
 # streamlit app
 st.title('Customer churn prediction for the Bank')
